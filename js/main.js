dff8331f6c9ed5ed94b2c4528114fde55af93f78
Jason Swetzoff <swetzoff@gmail.com>
2013-01-29 22:27:44 -0500

    Finished building out responsive header

--- js/main.js
+++ js/main.js
@@ -1,16 +1,6 @@
 
 // Main Menu Dropdown
 
+$("#form span.message").hide();
 
-$(document).ready(function() {
-
-	$('header #nav').removeClass('active');
-
-	$('header #main-toggle').click(function(){
-		$('header #nav').toggleClass('active');
-		// alert("Pressed");
-		return false;
-	});
-
-});
 
